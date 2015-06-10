6a3da8d859a7958ffaa94820b6271cff42655d6d
B <bibbi48@hotmail.it>
2015-06-10 12:36:24 +0200

    aggiunto metodo getOrderLinesByOrderId

--- src/it/uniroma3/model/OrderLineFacade.java
+++ src/it/uniroma3/model/OrderLineFacade.java
@@ -1,8 +1,12 @@
 package it.uniroma3.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 	@Stateless(name="orderLineFacade")
 	public class OrderLineFacade {
@@ -10,12 +14,18 @@ import javax.persistence.PersistenceContext;
 	    @PersistenceContext(unitName = "unit-jee-es2")
 	    private EntityManager em;
 	    
-	    public OrderLine createOrderLine(float unitPrice, int quantity){
-	    	OrderLine orderLine = new OrderLine(unitPrice, quantity);
+	    public OrderLine createOrderLine(float unitPrice, int quantity, Product product){
+	    	OrderLine orderLine = new OrderLine(unitPrice, quantity, product);
 	    	em.persist(orderLine);
 	    	return orderLine;
 	    }
 	    
+	    public List<OrderLine> getOrderLinesByOrderId(Long id){
+			Query q = em.createQuery("SELECT o FROM orderline o WHERE o.orders_id = :id");
+			q.setParameter("id", id);
+			List<OrderLine> orderLines = (ArrayList<OrderLine>) q.getResultList();
+			return orderLines;
+	    }
 		public EntityManager getEm() {
 			return em;
 		}
