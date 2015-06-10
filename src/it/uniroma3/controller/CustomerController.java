2784e0eb1dd933eb3912da40ba3fc7474e057f07
B <bibbi48@hotmail.it>
2015-06-10 12:22:46 +0200

    cancellata e riaggiunta esattamente com'è per risolvere alcuni bug

---
+++ src/it/uniroma3/controller/CustomerController.java
@@ -0,0 +1,117 @@
+package it.uniroma3.controller;
+
+import it.uniroma3.model.*;
+
+import java.util.List;
+
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+@ManagedBean
+public class CustomerController {
+	
+	@ManagedProperty(value="#{param.id}")
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Customer customer;
+	private String password;
+	private String email;
+	private List<Customer> customers;
+	
+	@EJB
+	private CustomerFacade customerFacade;
+	
+	public String createCustomer() {
+		this.customer = customerFacade.createCustomer(firstName, lastName);
+		return "customer"; 
+	}
+	
+	public String listCustomers() {
+		this.customers = customerFacade.getAllCustomers();
+		return "customers"; 
+	}
+
+	public String findCustomer() {
+		this.customer = customerFacade.getCustomer(id);
+		return "customer";
+	}
+	
+	public String findCustomer(Long id) {
+		this.customer = customerFacade.getCustomer(id);
+		return "customer";
+	}
+	
+	public String login(){
+		String nextPage = "";
+		try{
+		    this.customer = customerFacade.getCustomerByEmail(this.email);
+			if(customer.verifyPassword(this.password)){
+				nextPage = "customerWelcomePage";                                            //da implementare customer welcome page
+			} else nextPage = "customerLogin";
+		} 	
+		catch(Exception e){
+			nextPage = "customerLogin";
+		}
+		return nextPage;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<Customer> getCustomers() {
+		return this.customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
