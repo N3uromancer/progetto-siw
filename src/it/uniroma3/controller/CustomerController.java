4a11f4ee0f106c0df27909ceedf6123e257d8d60
igvar <igor_92007@bk.ru>
2015-06-12 19:15:27 +0200

    Risolto problema con @SessionScoped(cambiato import)

--- src/it/uniroma3/controller/CustomerController.java
+++ src/it/uniroma3/controller/CustomerController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
+import javax.enterprise.context.SessionScoped;
 
 @ManagedBean
 @SessionScoped
