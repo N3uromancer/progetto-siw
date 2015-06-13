f05e2531990bdec70fff494dbeb07a9653ad2598
B <bibbi48@hotmail.it>
2015-06-13 19:16:46 +0200

    commentato un import non utilizzato

--- src/it/uniroma3/controller/CustomerController.java
+++ src/it/uniroma3/controller/CustomerController.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
+//import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
